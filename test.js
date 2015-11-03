29eb48792b0f5cef43a86d0c123854acdbbe4a46
tchu01 <tchu01@calpoly.edu>
2015-11-03 00:40:16 -0800

    imagga Api files

--- test.js
+++ test.js
@@ -1,21 +1,55 @@
 var unirest = require("unirest");
+var AUTHORIZATION = "Basic YWNjXzE5ZmIzNzU1YzQ1M2I3ZjplMDg5ZjIyNGM1ZWE4MWVlZjY1MDZjYWI5OTIxOTA1OA==";
+var argvUrl = process.argv.slice(2);
 
-var req = unirest("GET", "http://api.imagga.com/v1/tagging");
+function Imagga() {
+};
 
-req.query({
-  "url": "https://logo.clearbit.com/linkedin.com",
-  "version": "2"
-});
+Imagga.prototype.tag = function tag(url, callback) {
+   var tagreq = unirest("GET", "http://api.imagga.com/v1/tagging");
 
-req.headers({
-  "authorization": "Basic YWNjX2Y4MGI3MTMxNjg4MDhlYjo0NjU4MGUxZjkyMzJjYWU2MjVhYTUyZDhjMjUyNTVjOQ==", 
-  "accept": "application/json"
-});
+   tagreq.query({
+      "url": url,
+      "version": "2"
+   });
 
+   tagreq.headers({
+      "authorization": AUTHORIZATION, 
+      "accept": "application/json"
+   });
 
-req.end(function (res) {
-  if (res.error) throw new Error(res.error);
+   tagreq.end(function (res) {
+      if (res.error) throw new Error(res.error);
+      var data = res.body;
+      var tagsdata = data['results'][0]['tags'];
+      for (var i = 0; i < tagsdata.length; i++) {
+         console.log(tagsdata[i]['tag']);
+      }
+   });
+};
 
-  console.log(res.body);
-});
+Imagga.prototype.color = function color(url) {
+   var colorreq = unirest("GET", "http://api.imagga.com/v1/colors");
 
+   colorreq.query({
+      "url": url,
+      "version": "2"
+   });
+
+   colorreq.headers({
+      "authorization": AUTHORIZATION,
+      "accept": "application/json"
+   });
+
+   colorreq.end(function (res) {
+      if (res.error) throw new Error(res.error);
+      var data = res.body;
+      console.log(JSON.stringify(data));
+   });
+};
+
+var test = new Imagga();
+test.tag(argvUrl);
+test.color(argvUrl);
+
+module.exports.magga = Imagga;
