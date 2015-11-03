649f3e39b844ec1610430fae7f07d56efceba139
Terrence Li <terrencezli@hotmail.com>
2015-11-03 10:10:13 -0800

    linkedin get companies json

--- LinkedInAPI.js
+++ LinkedInAPI.js
@@ -1,6 +1,5 @@
 'use strict';
 var request = require('request');
-var parseXML = require('xml2js').parseString;
 
 var APP_URL = 'https://api.linkedin.com/v1/company-search';
 
@@ -21,11 +20,8 @@ LinkedInAPI.prototype.apiCall = function (options, callback) {
     // Make http request call
     request.get(options, function(error, response, body) {
     	if (response.statusCode == 200 && !error) {
-	    	parseXML(body, function (err, val) {
-	    		//console.log(val['company-search']['companies'][0]['company'])
-	    		var result = val['company-search']['companies'][0]['company'];
-	    		callback(result);
-	    	});
+	    	var result = body['companies'];
+	    	callback(result);
 	    }
     });
 }
@@ -36,7 +32,7 @@ LinkedInAPI.prototype.apiCall = function (options, callback) {
 LinkedInAPI.prototype.getCompanies = function (industryCode, start, callback) {
 	var data = this.apiCall({
 		method: 'GET',
-		url: '?facet=industry,' + industryCode + '&start=' + start + '&count=20',
+		url: '?facet=industry,' + industryCode + '&start=' + start + '&count=20&format=json',
 		headers: {
 			Authorization: 'Bearer AQWz3I0GKRXObUqpWPLoFGeHmq1ug_6snY8tNJkmpnhuZ08DQeA7zgIRRCLvV2yKrYNjRIPWlweg4j4WHYCgPN_zqo3DwnQpFcjubrvGCo8z5MAqoVm3Be1_I7Tg9C-jGvXTCEF7lNBgA5I-ZCSwMbYtd52BIQ0-TsU_3pcY1VIsZa0_Ugk'
 		}
@@ -44,6 +40,21 @@ LinkedInAPI.prototype.getCompanies = function (industryCode, start, callback) {
 		callback(response);
 	});
 
+}
+
+/** 
+ * Get company websites
+ */
+LinkedInAPI.prototype.getWebsites = function (company, callback) {
+	var data = this.apiCall({
+		method: 'GET',
+		url: '?facet=industry,' + industryCode + '&start=' + start + '&count=20',
+		headers: {
+			Authorization: 'Bearer AQWz3I0GKRXObUqpWPLoFGeHmq1ug_6snY8tNJkmpnhuZ08DQeA7zgIRRCLvV2yKrYNjRIPWlweg4j4WHYCgPN_zqo3DwnQpFcjubrvGCo8z5MAqoVm3Be1_I7Tg9C-jGvXTCEF7lNBgA5I-ZCSwMbYtd52BIQ0-TsU_3pcY1VIsZa0_Ugk'
+		}
+	}, function(response) {
+		callback(response);
+	});
 }
 
 module.exports = LinkedInAPI;
--- sample.js
+++ sample.js
@@ -2,13 +2,16 @@ var LinkedIn = require('./LinkedInAPI');
 
 var abc = function () {
 	var linkedIn = new LinkedIn();
-	var start = 0;
 
-	for (; start <= 60; start+=20) {
-		linkedIn.getCompanies(118, start, function (response) {
+	linkedIn.getCompanies(118, 0, function (response) {
 			console.log(response);
 		});
-	}
+
+	// for (var start = 0; start <= 60; start+=20) {
+	// 	linkedIn.getCompanies(118, start, function (response) {
+	// 		console.log(response);
+	// 	});
+	// }
 }
 
 abc();
