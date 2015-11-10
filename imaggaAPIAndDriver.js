097d5a42c00bab91ffeefca30dabddbeb4c071de
tchu01 <tchu01@calpoly.edu>
2015-11-10 15:05:50 -0800

    11/10 after lab imagga work

---
+++ imaggaAPIAndDriver.js
@@ -0,0 +1,199 @@
+var unirest = require("unirest");
+var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+
+var AUTHORIZATION = "Basic YWNjXzE5ZmIzNzU1YzQ1M2I3ZjplMDg5ZjIyNGM1ZWE4MWVlZjY1MDZjYWI5OTIxOTA1OA==";
+var mongoURL = 'mongodb://pogomylogo:pogo1234pogo@162.243.144.203:27017/pogomylogo';
+
+function Imagga() {
+};
+
+var insertDoc = function(db, data, cb) {
+   db.collection('logos').insert(data, function(err, result) {
+      if(!err) {
+         console.log("Inserted into logos collection.");
+         cb();
+      }
+      else {
+        console.log(err);
+      }
+   });
+};
+
+var tag = function(db, doc, cb) {
+
+   var tagReq = unirest("GET", "http://api.imagga.com/v1/tagging");
+   console.log('tag doc: ' + JSON.stringify(doc));
+   console.log('tagging url: ' + doc.logoUrl);
+   tagReq.query({
+      "url": doc.logoUrl,
+      "version": "2"
+   });
+
+   tagReq.headers({
+      "authorization": AUTHORIZATION, 
+      "accept": "application/json"
+   });
+
+   tagReq.end(function (res) {
+      if (res.error) {
+         console.log('skippedtag');
+         console.log(res);
+         throw new Error(res.error);
+      }   
+
+      var data = res.body;
+      var tagData = data['results'][0]['tags'];
+      console.log('tagdata');
+
+      var temp = {
+         "name": doc.name,
+         "logoUrl": doc.logoUrl,
+         "tag": tagData
+      };
+      setTimeout(cb(db, doc, temp), 4000);
+/*
+      //put into database
+      insertDoc(db, temp, saveFinished);
+*/
+
+/*
+      for (var i = 0; i < tagsdata.length; i++) {
+         console.log(tagsdata[i]['tag']);
+      }
+*/
+   });
+};
+
+var color = function(db, doc, temp) {
+   var colorReq = unirest("GET", "http://api.imagga.com/v1/colors");
+
+   colorReq.query({
+      "url": doc.logoUrl,
+      "version": "2"
+   });
+
+   colorReq.headers({
+      "authorization": AUTHORIZATION,
+      "accept": "application/json"
+   });
+
+   colorReq.end(function (res) {
+      if (res.error) {
+         console.log("skippedcolor");
+         throw new Error(res.error);
+      }
+      var data = res.body;
+      console.log('colordata');
+   });
+   //cb();
+};
+
+var nextfield = function(db, doc) {
+   if (doc != null) {
+      //console.log(counter++);
+      var count = 2000;
+      //console.log('docname: ' + doc.name);
+      console.log('doc1:'+  doc);
+      console.log('doclogourl: ' + doc.logoUrl);
+      tag(db, doc, function(db, doc, temp) {
+         color(db, doc, temp, function(db, temp, saveFinished) {
+            insertDoc(db, temp, saveFinished)
+         });
+      });
+   }
+   
+}
+
+//Find urls in DB (put by LinkedIn API)
+Imagga.prototype.findURL = function findURL() {
+   MongoClient.connect(mongoURL, function(err, db) {
+      if(err) {
+         console.log(err);
+      }
+      else {
+         assert.equal(null, err);
+         console.log("Connected correctly to server.");
+
+         var cursor = db.collection('companies-test').find();
+         cursor.count(function(err,count) {
+
+            var savesPending = count;
+            console.log('Company count: ' + count);
+
+            //this tag IS working
+            //tag("http://playground.imagga.com/static/img/example_photo.jpg");
+
+            if(count == 0){
+               db.close();
+               return;
+            }
+
+            var saveFinished = function(){
+               savesPending--;
+               if(savesPending == 0){
+                  db.close();
+               }
+            }
+
+            //var counter = 0;
+            while(cursor.hasNext()) {
+            	console.log(cursor.nextObject.toString());
+                cursor.nextObject(function(err, doc) {
+                	if (doc != null) {
+                  		//console.log(counter++);
+                  		//console.log('docname: ' + doc.name);
+                  		console.log('doc1:'+  doc);
+                  		console.log('doclogourl: ' + doc.logoUrl);
+                  		tag(db, doc, function(db, doc, temp) {
+                    		color(db, doc, temp, function(db, temp, saveFinished) {
+                    			insertDoc(db, temp, saveFinished)
+                     		});
+                		});
+                	}
+                	else {
+                		console.log(doc === null);
+                	}
+				});                
+            } 
+/*
+            cursor.each(function (err, doc) {
+               if (doc != null) {
+                  //console.log(counter++);
+                  var count = 2000;
+                  //console.log('docname: ' + doc.name);
+                  console.log('doc1:'+  doc);
+                  console.log('doclogourl: ' + doc.logoUrl);
+                  tag(db, doc, function(db, doc, temp) {
+                     color(db, doc, temp, function(db, temp, saveFinished) {
+                        insertDoc(db, temp, saveFinished)
+                     });
+                  });
+               setTimeout(null, 20000);
+*/
+   /* 
+                  var temp = {
+                     "name": doc.name,
+                     "logoUrl": doc.logoUrl//,
+                     //"tag": tagData,
+                     //"color": colorData
+                  };
+                  //put into database
+                  insertDoc(db, temp, saveFinished);
+    */
+              // }
+           // });
+
+         });
+      }
+   });
+};
+
+
+var test = new Imagga();
+test.findURL();
+//tag("http://playground.imagga.com/static/img/example_photo.jpg");
+
+
+module.exports.imagga = Imagga;
