f60a496c77d8a0693838fceda757c74c56534258
Aaron Pramana <aaron@sociotopia.com>
2015-11-25 15:36:53 -0500

    last fix to restore

--- api/server.js
+++ api/server.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 var app        = express();
 var morgan     = require('morgan');
 
+var MongoClient = require('mongodb').MongoClient;
+
 // configure app
 app.use(morgan('dev')); // log requests to the console
 
@@ -17,7 +19,7 @@ app.use(bodyParser.json());
 var port     = process.env.PORT || 8080; // set our port
 
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
+mongoose.connect('mongodb://127.0.0.1:27017/pogomylogo'); // connect to our database
 var Bear     = require('./app/models/bear');
 
 // ROUTES FOR OUR API
@@ -28,88 +30,105 @@ var router = express.Router();
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
-	// do logging
-	console.log('Something is happening.');
-	next();
+   // do logging
+   console.log('Something is happening.');
+   next();
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });	
+   res.json({ message: 'hooray! welcome to our api!' }); 
 });
 
-// on routes that end in /bears
-// ----------------------------------------------------
-router.route('/bears')
+// getSuggestions
+router.route('/suggestion')
 
-	// create a bear (accessed at POST http://localhost:8080/bears)
-	.post(function(req, res) {
-		
-		var bear = new Bear();		// create a new instance of the Bear model
-		bear.name = req.body.name;  // set the bears name (comes from the request)
+   .get(function(req, res) {
+      
+      industry = req.params.industry;
+      companySize = req.params.companySize;
 
-		bear.save(function(err) {
-			if (err)
-				res.send(err);
+      // do some processing, create the suggestion object
+      suggestion = {color: "#FF0000", numColors: 3, tags: ["sun", "bright", "ocean"]};
+      res.json({
+         color: suggestion.color,
+         numColors: suggestion.numColors,
+         tags: suggestion.tags
 
-			res.json({ message: 'Bear created!' });
-		});
+      });
 
-		
-	})
+      
+   });
 
-	// get all the bears (accessed at GET http://localhost:8080/api/bears)
-	.get(function(req, res) {
-		Bear.find(function(err, bears) {
-			if (err)
-				res.send(err);
 
-			res.json(bears);
-		});
-	});
+// getRelatedTags
+router.route('/tag/:userTag')
+   .get(function(req, res) {
+      var url = 'mongodb://127.0.0.1:27017/pogomylogo';
+      MongoClient.connect(url, function(err, db) {
+         userTag = req.params.userTag;
+         cur = db.collection('logos-tag').find({tag: {$elemMatch: {tag: userTag}}});
+         //console.log(cur.count());
+         var popTags = {};
+         cur.toArray(function(err, docs) {
+            //if(doc != null){
+            for(j = 0; j < docs.length; j++) {
+            company = docs[j];
+            console.log(popTags[company.tag[0].tag]);
+            for(i = 0; i < 3; i++) {
+               if(popTags[company.tag[i].tag] === undefined && company.tag[i].confidence > 10)
+                  popTags[company.tag[i].tag] = company.tag[i].confidence;
+            }
+            }
+            //}
+            console.log(popTags);
+            res.json(popTags);
+         });
+      });
+   });
 
 // on routes that end in /bears/:bear_id
 // ----------------------------------------------------
-router.route('/bears/:bear_id')
+/*router.route('/bears/:bear_id')
 
-	// get the bear with that id
-	.get(function(req, res) {
-		Bear.findById(req.params.bear_id, function(err, bear) {
-			if (err)
-				res.send(err);
-			res.json(bear);
-		});
-	})
+   // get the bear with that id
+   .get(function(req, res) {
+      Bear.findById(req.params.bear_id, function(err, bear) {
+         if (err)
+            res.send(err);
+         res.json(bear);
+      });
+   })
 
-	// update the bear with this id
-	.put(function(req, res) {
-		Bear.findById(req.params.bear_id, function(err, bear) {
+   // update the bear with this id
+   .put(function(req, res) {
+      Bear.findById(req.params.bear_id, function(err, bear) {
 
-			if (err)
-				res.send(err);
+         if (err)
+            res.send(err);
 
-			bear.name = req.body.name;
-			bear.save(function(err) {
-				if (err)
-					res.send(err);
+         bear.name = req.body.name;
+         bear.save(function(err) {
+            if (err)
+               res.send(err);
 
-				res.json({ message: 'Bear updated!' });
-			});
+            res.json({ message: 'Bear updated!' });
+         });
 
-		});
-	})
+      });
+   })
 
-	// delete the bear with this id
-	.delete(function(req, res) {
-		Bear.remove({
-			_id: req.params.bear_id
-		}, function(err, bear) {
-			if (err)
-				res.send(err);
+   // delete the bear with this id
+   .delete(function(req, res) {
+      Bear.remove({
+         _id: req.params.bear_id
+      }, function(err, bear) {
+         if (err)
+            res.send(err);
 
-			res.json({ message: 'Successfully deleted' });
-		});
-	});
+         res.json({ message: 'Successfully deleted' });
+      });
+   });*/
 
 
 // REGISTER OUR ROUTES -------------------------------
